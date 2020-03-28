4a413d3b3636f1c0e3995e041053bd04e40dde80
Walter Sylvester <walter_sylvester@msn.com>
2020-03-28 16:37:32 -0600

    index-3.js works as expected...meta user-scalable=no

--- src/Sticky/version-5.js
+++ src/Sticky/version-5.js
@@ -1,17 +1,20 @@
+// This does not work...the overlay fixed column does not do what I want
+
 import _ from 'lodash';
 import React, { Component, createRef } from 'react';
 import { Icon, Label, Menu, Table, Visibility } from 'semantic-ui-react';
 
 
-const overlayStyle = { // for the Header Row
+const overlayStyleHeaderRow = { // for the Header Row
   display: 'none', // Hidden by default
 }
 
-const fixedOverlayStyle = { // for the Header Row, initial style
-  ...overlayStyle,
+const fixedOverlayStyleHeaderRow = { // for the Header Row, initial style
+  ...overlayStyleHeaderRow,
   position: '-webkit-sticky', 
-  position: 'sticky',
+  position: 'sticky', // position sticky works on <th>
   top: 0,
+  zIndex: 10,
   // border: '3px solid blue',
   display: 'block', // Show it
 }
@@ -28,9 +31,36 @@ const fixedOverlayHeaderRowStyle = { // updates style
   color: 'white',
 }
 
+const overlayStyleFirstColumn = { // for the First Column
+  display: 'none', // Hidden by default
+}
+
+const fixedOverlayStyleFirstColumn = { // for the First Column, initial style
+  ...overlayStyleFirstColumn,
+  // position: '-webkit-sticky', 
+  position: 'fixed',
+  left: 0,
+  zIndex: 1,
+  border: '3px solid black',
+  display: 'block', // Show it
+}
+
+const overlayFirstColumnStyle = { 
+  display: 'none', // Hidden by default
+}
+
+const fixedOverlayFirstColumnStyle = { // updates style
+  ...overlayFirstColumnStyle,
+  display: 'inline', // Show it
+  border: '1px solid gold',
+  backgroundColor: 'pink',
+  color: 'white',
+}
+
 const TableCell = () => (
   <Table.Cell className='table-cell'>Cell</Table.Cell>
 )
+
 const TableRow = () =>  (
   <Table.Row className='table-row'>
     <Table.Cell className='table-cell'>FirstFixed</Table.Cell>
@@ -39,6 +69,8 @@ const TableRow = () =>  (
           ))}
   </Table.Row>
 )
+
+
 
 // https://www.geeksforgeeks.org/reactjs-importing-exporting/
 // const TableExamplePagination = () => (
@@ -67,20 +99,57 @@ class TableFreezeHeaderColumn extends Component {
   constructor() {
     super();
     this.state = {
-      overlayFixed: false,
+      overlayFixedHeader: false,
+      overlayFixedFirstColumn: false,
+      prevScrollPos: window.pageXOffset,// starting position for detect horizontal scroll
     }
   }
   // contextRef = createRef()
 
-  stickOverlayTableHeader = () => this.setState({ overlayFixed: true })
-  unStickOverlayTableHeader = () => this.setState({ overlayFixed: false })
+  stickOverlayTableHeader = () => this.setState({ overlayFixedHeader: true })
+  unStickOverlayTableHeader = () => this.setState({ overlayFixedHeader: false })
+  
+  // Hide or show the column overlay based on horizontal scroll
+  detectHorizontalScroll = () => { // https://dev.to/guimg/hide-menu-when-scrolling-in-reactjs-47bj
+    // if the viewport moves left or right and does not move up or down, then that is a horizontal scroll
+    const { prevScrollPos } = this.state;
+
+    const currentScrollPos = window.pageXOffset;
+    const overlayFixedFirstColumn = 
+      (currentScrollPos - prevScrollPos !== 0) ? true : false
+
+    this.setState({
+      prevScrollPos: currentScrollPos,
+      overlayFixedFirstColumn
+    })
+  }
+
+  // Adds an event listener when the component is mount.
+  componentDidMount() {
+    window.addEventListener("scroll", this.detectHorizontalScroll);
+  }
+
+  // Remove the event listener when the component is unmount.
+  componentWillUnmount() {
+    window.removeEventListener("scroll", this.detectHorizontalScroll);
+  }
+
+  // stickOverlayFirstColumn = () => this.setState({ 
+  //   overlayFixedFirstColumn: visibile == true ? true : undefined 
+  //   })
+  // unStickOverlayFirstColumn = () => this.setState({ 
+  //   overlayFixedFirstColumn: visible === false ? false : undefined
+  //   })
 
 
   render() {
     
-    const { overlayFixed } = this.state
-    console.log(overlayFixed); // this is a static false in mobile due to layout/virtual viewport versus visual viewport
-                               // https://developer.mozilla.org/en-US/docs/Mozilla/Mobile/Viewport_meta_tag
+    const { overlayFixedHeader, overlayFixedFirstColumn } = this.state
+    console.log('overlayFixedHeader: ', overlayFixedHeader);
+    console.log('overlayFixedFirstColumn: ', overlayFixedFirstColumn); 
+    // overlayFixedHeader was static false in mobile due to layout/virtual viewport versus visual viewport
+    // https://developer.mozilla.org/en-US/docs/Mozilla/Mobile/Viewport_meta_tag
+    
 
     const HeaderRow = () => (
       <Table.HeaderCell
@@ -92,26 +161,43 @@ class TableFreezeHeaderColumn extends Component {
     const HeaderRowOverlay = () => (
       <Table.HeaderCell
         className='header-cell-overlay'
-        style={overlayFixed ? fixedOverlayHeaderRowStyle : overlayHeaderRowStyle}
-      >Header
+        style={overlayFixedHeader ? fixedOverlayHeaderRowStyle : overlayHeaderRowStyle}
+        >Header
       </Table.HeaderCell>
     )    
 
+    const FirstColumnOverlay = () => (
+      <Table.Row className='table-row'>
+        <Table.Cell 
+          className='first-column-cell'
+          style={overlayFixedFirstColumn ? fixedOverlayFirstColumnStyle : overlayFirstColumnStyle }
+          >FirstFixed
+        </Table.Cell>
+      </Table.Row>
+    )
+
+
     return(
       <React.Fragment>
-      <title>{`Test`}</title>
-        {/*<Ref innerRef={this.contextRef}>*/}
+        {/*It seems like the default body is overflow-x: hidden;*/}
+        <style>
+          {`
+            body {
+              overflow: visible;
+            }
+          `}
+        </style>
+        
             <div 
-              // ref={this.contextRef}
-              style={overlayFixed ? fixedOverlayStyle : overlayStyle}
+              style={overlayFixedHeader ? fixedOverlayStyleHeaderRow : overlayStyleHeaderRow}
             >
               <Table celled unstackable>
                 <Table.Header>
                   <Table.Row className='table-header-row'>
                     <Table.HeaderCell
                       className='header-cell-overlay'
-                      style={overlayFixed ? fixedOverlayHeaderRowStyle : overlayHeaderRowStyle}
-                    >HeaderFixed
+                      style={overlayFixedHeader ? fixedOverlayHeaderRowStyle : overlayHeaderRowStyle}
+                      >HeaderFixed
                     </Table.HeaderCell>
                     {_.times(20, (i) => (
                       <HeaderRowOverlay key={i} />
@@ -121,22 +207,33 @@ class TableFreezeHeaderColumn extends Component {
               </Table>
             </div>
 
+            <div
+              style={overlayFixedFirstColumn ? fixedOverlayStyleFirstColumn : overlayStyleFirstColumn}
+            >
+              <Table celled unstackable>
+                <Table.Body>
+                {_.times(25, (i) => (
+                    <TableRow key={i} />
+                  ))} 
+                </Table.Body>
+              </Table>
+            </div>
+
             <div>
             <Visibility
               // offset={0}
               onTopPassed={this.stickOverlayTableHeader}
               onTopVisible={this.unStickOverlayTableHeader}
               once={false}
+              // onPassing={this.stickOverlayFirstColumn}
+              // onOffScreen={this.unStickOverlayFirstColumn}
             >
             
               <Table celled unstackable>        
                 
                 <Table.Header>
                   <Table.Row className='table-header-row'>
-                    <Table.HeaderCell
-                      className='header-cell'
-                      >HeaderFixed
-                    </Table.HeaderCell>
+                    <Table.HeaderCell className='header-cell'>HeaderFixed</Table.HeaderCell>
                     {_.times(20, (i) => (
                       <HeaderRow key={i} />
                     ))}
@@ -155,7 +252,6 @@ class TableFreezeHeaderColumn extends Component {
             </div>
             
             
-        {/*</Ref>*/}
       </React.Fragment>
     )
 
---
+++ src/Table/index-3.js
@@ -0,0 +1,65 @@
+import _ from 'lodash';
+import React, { Component } from 'react';
+import { Table } from 'semantic-ui-react';
+import './style-3.css';
+
+
+const HeaderRow = () => (
+  <Table.HeaderCell className='header-cell'>Header</Table.HeaderCell>
+)
+const TableCell = () => (
+  <Table.Cell className='table-cell'>Cell</Table.Cell>
+)
+const TableRow = () =>  (
+  <Table.Row className='table-row'>
+    <Table.Cell className='table-cell first-column'>FirstFixed</Table.Cell>
+    {_.times(20, (i) => (
+		<TableCell key={i} />
+	))}
+  </Table.Row>
+)
+
+
+class TableFreezeHeaderColumn extends Component {
+  constructor() {
+    super();
+    this.state = {
+      
+    }
+  }
+
+
+  render() {
+ 	let vh = window.innerHeight;
+ 	let vw = window.innerWidth;
+ 	console.log(vh + ' x '+ vw);
+
+    return(
+      <React.Fragment>
+      	
+          <Table celled unstackable className='freeze-head-and-col'>        
+            <Table.Header>
+              <Table.Row className='header-row'>
+                <Table.HeaderCell className='header-cell'>HeaderFixed</Table.HeaderCell>
+                {_.times(20, (i) => (
+	            	<HeaderRow key={i} />
+	            ))}
+              </Table.Row>
+            </Table.Header>        
+
+            <Table.Body>
+	        	{_.times(25, (i) => (
+	        		<TableRow key={i} />
+	        	))}
+            </Table.Body>
+          
+          </Table>
+      </React.Fragment>
+    )
+
+  }
+
+}
+
+
+export default TableFreezeHeaderColumn;
